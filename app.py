762149803e325425276c574c0d78a145fd1a6730
Tom Smail <thomas.c.smail@gmail.com>
2025-01-10 14:12:44 +0000

    feat: create app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, request, jsonify
+
+# Initialize the Flask app
+app = Flask(__name__)
+
+# Homepage route
+@app.route('/')
+def home():
+    return "<h1>Hello World</h1>"
+
+# Run the app
+if __name__ == '__main__':
+    app.run(debug=True)
