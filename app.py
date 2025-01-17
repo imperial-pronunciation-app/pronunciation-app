111c55abed28c02b2f7cc1d6d9c68938f1bc4728
James Watling <james.watling@gmail.com>
2025-01-17 12:00:27 +0000

    Small app fixes

--- app.py
+++ app.py
@@ -19,8 +19,8 @@ session = Session()
 
 class User(Base):
     __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), nullable=False)
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), nullable=False)
 
     def __repr__(self):
         return f'<User {self.name}>'
