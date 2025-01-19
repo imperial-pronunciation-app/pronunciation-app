bc883e71fc2b6a7f619474d72fa702cd20cd6bcf
Euan Turner <euanoturner@gmail.com>
2025-01-19 18:29:19 +0000

    feat(API): :construction: Draft random word response for test

--- app/routers/random_word.py
+++ app/routers/random_word.py
@@ -1,18 +1,31 @@
-from fastapi import APIRouter
+import random
+
+from fastapi import APIRouter, Depends, HTTPException
 from schemas.random_word import RandomWord, WordPhoneme
+from sqlmodel import Session
+
+from app.database import get_db
+from app.models import Phoneme, Word, WordPhonemes
 
 router = APIRouter()
 
 
 @router.get("/api/v1/random_word", response_model=RandomWord)
-async def get_random_word():
-    return RandomWord(
-        word_id=1,
-        word="hello",
-        word_phonemes=[
-            WordPhoneme(id=1, ipa="h", respelling="h"),
-            WordPhoneme(id=2, ipa="ɛ", respelling="e"),
-            WordPhoneme(id=3, ipa="l", respelling="l"),
-            WordPhoneme(id=4, ipa="oʊ", respelling="o"),
-        ],
+async def get_random_word(db: Session = Depends(get_db)):
+    words = db.exec(Word).all()
+    if not words:
+        raise HTTPException(status_code=404, detail="No words found")
+
+    random_word = random.choice(words)
+
+    # TODO: This can definitely be simplified
+    word_phoneme_entries = (
+        db.exec(WordPhonemes).filter(WordPhonemes.word_id == random_word.id).order_by(WordPhonemes.index).all()
     )
+    phoneme_ids = [entry.phoneme_id for entry in word_phoneme_entries]
+    word_phonemes = db.exec(Phoneme).filter(Phoneme.id.in_(phoneme_ids)).all()
+    word_phonemes = [
+        WordPhoneme(phoneme_id=phoneme.id, ipa=phoneme.ipa, respelling=phoneme.respelling) for phoneme in word_phonemes
+    ]
+
+    return RandomWord(word_id=random_word.id, word=random_word.word, word_phonemes=word_phonemes)
