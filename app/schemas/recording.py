a3c6331976a00292bf70a8246f6f732798b1ae06
Euan Turner <euanoturner@gmail.com>
2025-01-19 14:16:26 +0000

    fix(Schemas): :bug: Correct schema filenames

--- app/schemas/recording.py
+++ app/schemas/recording.py
@@ -1,5 +1,19 @@
+from typing import List
+
 from pydantic import BaseModel
 
 
-class SatisfactionRequest(BaseModel):
-    satisfied: bool
+class RecordingRequest(BaseModel):
+    recording: bytes
+
+
+class RecordingPhoneme(BaseModel):
+    id: int
+    ipa: str
+    respelling: str
+
+
+class Feedback(BaseModel):
+    recording_id: int
+    score: int
+    recording_phonemes: List[RecordingPhoneme]
