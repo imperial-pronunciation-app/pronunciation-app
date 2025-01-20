dddc16f478fe7c31e4dc80ea8f09408d4a24adf6
James Watling <james.watling@gmail.com>
2025-01-20 18:35:31 +0000

    refactor: Clean up model, router, and schema initializations; remove unused imports and files

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -1,12 +1,1 @@
 # Contains routes and endpoints
-
-# TODO: Would we rather just have one router with all the endpoints?
-from .random_word import router as random_word_router
-from .recording import router as recording_router
-from .satisfaction import router as satisfaction_router
-
-__all__ = [
-    "random_word_router",
-    "recording_router",
-    "satisfaction_router",
-]
