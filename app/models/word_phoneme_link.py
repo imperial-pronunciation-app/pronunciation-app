13257d0a7534a53b94889187d004fc04e5f41195
Jack Nguyen <trungnb2210@gmail.com>
2025-01-21 17:13:58 +0000

    feat(random_word): :sparkles: Define word, phoneme, wordphonemelink models and test database creation with these models

--- app/models/word_phoneme_link.py
+++ app/models/word_phoneme_link.py
@@ -1,9 +1,7 @@
 from typing import Optional
-
 from sqlmodel import Field, SQLModel
 
-
-class WordPhonemeLink(SQLModel, table=True):
+class WordPhonemeLink(SQLModel, table = True):
     word_id: Optional[int] = Field(foreign_key="word.id", primary_key=True)
     phoneme_id: Optional[int] = Field(foreign_key="phoneme.id", primary_key=True)
-    index: int = Field(primary_key=True) # needs to be a primary key in case a word contains multiples of the same phoneme
+    index: int
