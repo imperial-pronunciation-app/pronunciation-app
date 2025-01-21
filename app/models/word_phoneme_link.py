7c769a8d66c00aacc8eb479ea7d8c99e1027f9c3
Euan Turner <euanoturner@gmail.com>
2025-01-21 22:03:01 +0000

    fix(Models): :bug: Use index as part of primary key for WordPhonemeLink
    Needed to identify unique instances of occurrences of a phoneme within a word. For example, "r" occurs twice in "hardware"

--- app/models/word_phoneme_link.py
+++ app/models/word_phoneme_link.py
@@ -6,4 +6,4 @@ from sqlmodel import Field, SQLModel
 class WordPhonemeLink(SQLModel, table=True):
     word_id: Optional[int] = Field(foreign_key="word.id", primary_key=True)
     phoneme_id: Optional[int] = Field(foreign_key="phoneme.id", primary_key=True)
-    index: int
+    index: int = Field(primary_key=True) # needs to be a primary key in case a word contains multiples of the same phoneme
