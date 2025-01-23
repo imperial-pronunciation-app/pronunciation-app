82f945609a2a84a51765401bce69e6850a474dd8
Euan Turner <euanoturner@gmail.com>
2025-01-23 20:20:13 +0000

    chore: :card_file_box: Add more words to seed

--- app/seed_data.py
+++ app/seed_data.py
@@ -82,4 +82,10 @@ class SeedData:
 default_data = SeedData(words=[
         WordData(word="software", phonemes=["s", "oʊ", "f", "t", "w", "ɛ", "r"]),
         WordData(word="hardware", phonemes=["h", "ɑː", "r", "d", "w", "ɛ", "r"]),
+        WordData(word="computer", phonemes=["k", "ə", "m", "p", "j", "u:", "t", "ə"]),
+        WordData(word="compilers", phonemes=["k", "ə", "m", "p", "aɪ", "l", "ə", "r"]),
+        WordData(word="keyboard", phonemes=["k", "i:", "b", "ɔː", "d"]),
+        WordData(word="mouse", phonemes=["m", "aʊ", "s"]),
+        WordData(word="parrot", phonemes=["p", "æ", "r", "ə", "t"]),
+        WordData(word="chocolate", phonemes=["tʃ", "ɒ", "k", "l", "ə", "t"]),
     ])
