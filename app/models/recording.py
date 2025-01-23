a937ac658ecc528df58c6833ebf18507d8569cc5
Euan Turner <euanoturner@gmail.com>
2025-01-23 18:01:53 +0000

    fix(RecordingEndpoint): :bug: Specify word fk properly

--- app/models/recording.py
+++ app/models/recording.py
@@ -13,5 +13,5 @@ class Recording(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     recording_s3_key: str
     # user_id: int = Field(foreign_key="user.id")
-    word_id: int = Field(foreign_key="phoneme.id")
+    word_id: int = Field(foreign_key="word.id")
     time_created: datetime
