57d599abe011c687f102d259c9faf7c84cca0288
James Watling <james.watling@gmail.com>
2025-01-31 14:03:24 +0000

    feat: integrate phoneme inference model API and update recording logic

--- app/main.py
+++ app/main.py
@@ -1,25 +1,14 @@
-from contextlib import asynccontextmanager
-from typing import AsyncGenerator, Dict
+from typing import Dict
 
 from fastapi import FastAPI
-from transformers import pipeline
 
 from app.routers.auth import router as auth_router
 from app.routers.random_word import router as random_word_router
-from app.routers.recording import ml_models
 from app.routers.recording import router as recording_router
 from app.routers.users import router as users_router
 
 
-@asynccontextmanager
-async def lifespan(app: FastAPI) -> AsyncGenerator[None, None]:
-    # Before app startup
-    ml_models["whisper"] = pipeline(task="automatic-speech-recognition", model="openai/whisper-tiny.en")
-    yield
-    # After app shutdown
-    ml_models.clear()
-
-app = FastAPI(lifespan=lifespan)
+app = FastAPI()
 
 
 @app.get("/")
--- app/routers/recording.py
+++ app/routers/recording.py
@@ -1,28 +1,24 @@
 import os
-import string
 import uuid
-from typing import Dict
 
-import dotenv
-from fastapi import APIRouter, Depends, HTTPException, UploadFile
-from sqlmodel import Session
-from transformers.pipelines import Pipeline
+import requests
+from fastapi import APIRouter, Depends, UploadFile
+from sqlmodel import Session, col, select
 
+from app.config import get_settings
 from app.crud.recording_repository import RecordingRepository
 from app.database import get_session
+from app.models.phoneme import Phoneme
 from app.models.user import User
-from app.models.word import Word
+from app.models.word_phoneme_link import WordPhonemeLink
+from app.schemas.model_api import InferPhonemesResponse
 from app.schemas.recording import RecordingRequest, RecordingResponse
 from app.users import current_active_user
 from app.utils.s3 import upload_wav_to_s3
 from app.utils.similarity import similarity
 
 
-dotenv.load_dotenv()
-
 router = APIRouter()
-
-ml_models: Dict[str, Pipeline] = {}
 
 def create_wav_file(recording_request: RecordingRequest) -> str:
     temp_file = uuid.uuid4()
@@ -31,15 +27,18 @@ def create_wav_file(recording_request: RecordingRequest) -> str:
         f.write(recording_request.audio_bytes)
     return filename
 
-def dispatch_to_model(wav_file: str) -> str:
-    # TODO: Future models will return a list of phonemes
-    model = ml_models["whisper"]
-    return (
-        str(model(wav_file)["text"])
-        .lower()
-        .strip()
-        .translate(str.maketrans("", "", string.punctuation)) # Remove punctuation
-    )
+def dispatch_to_model(wav_file: str) -> list[str]:
+    files = {
+        "audio_file": ("audio.wav", open(wav_file, "rb"), "audio/wav")
+    }
+
+    print(get_settings().MODEL_API_URL)
+    model_response = requests.post(f"{get_settings().MODEL_API_URL}/api/v1/infer_phonemes", files=files)
+    model_response.raise_for_status()
+
+    model_data = InferPhonemesResponse.model_validate(model_response.json())
+
+    return model_data.phonemes
 
 @router.post("/api/v1/words/{word_id}/recording", response_model=RecordingResponse)
 async def post_recording(
@@ -60,14 +59,23 @@ async def post_recording(
     recording = recording_repository.create(word_id, s3_key, user.id)
     
     # 3. Dispatch recording to ML backend
-    model_response = dispatch_to_model(wav_file)
+    inferred_phonemes = dispatch_to_model(wav_file)
     
     # 4. Form feedback based on model response
-    word = session.get(Word, word_id)
-    if not word:
-        raise HTTPException(status_code=404, detail="Word not found")
-    print(word)
-    feedback = similarity(word.word, model_response)
+    phoneme_query = (
+        select(Phoneme)
+        .join(WordPhonemeLink)
+        .where(WordPhonemeLink.word_id == word_id)
+        .order_by(col(WordPhonemeLink.index))
+        )
+    phonemes = session.exec(phoneme_query).all()
+
+    word_phonemes = []
+    for phoneme in phonemes:
+        assert phoneme.id is not None
+        word_phonemes.append(phoneme.ipa)
+
+    feedback = similarity(word_phonemes, inferred_phonemes)
     
     # 5. TODO: Store feedback in RecordingFeedback
     
