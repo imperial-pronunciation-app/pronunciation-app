b110805f079188dd4e2040507f4e270a0d2d1b54
Henry Yu <henrytsyu@gmail.com>
2025-02-02 02:38:23 +0000

    test: leaderboard endpoint

--- tests/test_leaderboard.py
+++ tests/test_leaderboard.py
@@ -1,108 +1,32 @@
-# import pytest
-# from sqlmodel import Session
+from fastapi.testclient import TestClient
+from sqlmodel import Session
 
-# from app.crud.leaderboard_user_repository import LeaderboardUserRepository
-# from app.models.leaderboard_user import League
-
-
-# @pytest.fixture(name="repository")
-# def leaderboard_repo(session: Session) -> LeaderboardUserRepository:
-#     return LeaderboardUserRepository(session)
+from app.models.leaderboard_user import LeaderboardUser, League
+from app.models.user import User
+from tests.utils import TEST_EMAIL, TEST_PASSWORD
 
 
-# def test_update_xp_no_record(repository: LeaderboardUserRepository) -> None:
-#     # Pre
-#     user_id = 1
-#     league = League.GOLD
-#     xp_gain = 100
-
-#     # When
-#     result = repository.update_xp(user_id, league, xp_gain)
-
-#     # Then
-#     assert result.user_id == user_id
-#     assert result.league == league
-#     assert result.xp == xp_gain
-
-#     # When
-#     result2 = repository.get_user_entry(user_id)
-
-#     # Then
-#     assert result2
-#     assert result2.user_id == user_id
-#     assert result2.league == league
-#     assert result2.xp == xp_gain
-
-
-# def test_update_xp_existing_record(repository: LeaderboardUserRepository) -> None:
-#     # Pre
-#     user_id = 1
-#     league = League.GOLD
-#     xp_gain = 100
-
-#     # When
-#     result = repository.update_xp(user_id, league, xp_gain)
-
-#     # Then
-#     assert result.user_id == user_id
-#     assert result.league == league
-#     assert result.xp == xp_gain
-
-#     # Pre
-#     xp_gain = 50
-
-#     # When
-#     result2 = repository.update_xp(user_id, league, xp_gain)
-
-#     # Then
-#     assert result2.user_id == user_id
-#     assert result2.league == league
-#     assert result2.xp == 150
-
-#     # When
-#     result3 = repository.get_user_entry(user_id)
-
-#     # Then
-#     assert result3
-#     assert result3.user_id == user_id
-#     assert result3.league == league
-#     assert result3.xp == 150
-
-
-# def test_get_current_leaderboard(repository: LeaderboardUserRepository) -> None:
-#     # Pre
-#     league = League.GOLD
-#     user_ids = [i for i in range(6)]
-#     xp_gains = [100, 50, 200, 75, 150, 25]
-#     for user_id, xp_gain in zip(user_ids, xp_gains):
-#         repository.update_xp(user_id, league, xp_gain)
+def test_get_leaderboard_empty_user_record(session: Session, authorised_client: TestClient) -> None:
+    leaders = [User(
+        email=f"example{i}@gmail.com",
+        hashed_password=TEST_PASSWORD,
+        is_active=True,
+        is_superuser=False,
+        is_verified=True,
+    ) for i in range(10)]
+    session.add_all(leaders)
+    session.flush()
+    for i, leader in enumerate(leaders):
+        session.refresh(leader)
+        session.add(LeaderboardUser(user_id=leader.id, xp=i*2))
     
-#     # When
-#     result = repository.get_global_leaderboard(league)
-
-#     # Then
-#     assert len(result) == 5
-#     assert result[0].xp == 200
-#     assert result[1].xp == 150
-#     assert result[2].xp == 100
-#     assert result[3].xp == 75
-#     assert result[4].xp == 50
-
-
-# def test_reset_leaderboard(repository: LeaderboardUserRepository) -> None:
-#     # Pre
-#     league = League.GOLD
-#     user_ids = [i for i in range(3)]
-#     xp_gains = [100, 50, 200]
-#     for user_id, xp_gain in zip(user_ids, xp_gains):
-#         repository.update_xp(user_id, league, xp_gain)
-    
-#     # When
-#     repository.reset_leaderboard()
-
-#     # Then
-#     result = repository.get_global_leaderboard(league)
-#     assert not result
-#     for user_id in user_ids:
-#         entry = repository.get_user_entry(user_id)
-#         assert not entry
+    response = authorised_client.get("/api/v1/leaderboard/global")
+    assert response.status_code == 200
+    json = response.json()
+    assert json["league"] == League.BRONZE
+    assert json["leaders"] == [
+        {"rank": 1, "username": "example9@gmail.com", "xp": 18},
+        {"rank": 2, "username": "example8@gmail.com", "xp": 16},
+        {"rank": 3, "username": "example7@gmail.com", "xp": 14},
+    ]
+    assert json["current"] == [{"rank": 0, "username": TEST_EMAIL, "xp": 0}]
