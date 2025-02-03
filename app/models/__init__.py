7d8fd21d5d2aeb237ee92b97058cb9a183322197
James Watling <james.watling@gmail.com>
2025-02-03 16:58:29 +0000

    feat: include LeaderboardUser and User models in exports and seed script (#44)

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,10 +1,10 @@
 # Contains SQLModel models for database tables
+from .leaderboard_user import LeaderboardUser
 from .phoneme import Phoneme
 from .recording import Recording
-
-# from .user import User
+from .user import User
 from .word import Word
 from .word_phoneme_link import WordPhonemeLink
 
 
-__all__ = ["Word", "Phoneme", "WordPhonemeLink", "Recording"]
+__all__ = ["Word", "Phoneme", "WordPhonemeLink", "Recording", "LeaderboardUser", "User"]
