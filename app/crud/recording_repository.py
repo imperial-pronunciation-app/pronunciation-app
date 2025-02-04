bb9312293798778c41fca1137e8ecd8d2d4d9398
Pedro Sa Fontes <pedrosafontes.psf@gmail.com>
2025-02-04 21:11:47 +0000

    refactor: remove create method from RecordingRepository

--- app/crud/recording_repository.py
+++ app/crud/recording_repository.py
@@ -11,10 +11,3 @@ class RecordingRepository(GenericRepository[Recording]):
     def __init__(self, session: Session) -> None:
         super().__init__(session, Recording)
     
-    def create(self, attempt_id: int, s3_key: str) -> Recording:
-        recording = Recording(
-            attempt_id=attempt_id,
-            recording_s3_key=s3_key,
-        )
-        return self.upsert(recording)
-    
