cd470d180d6f55f32d260627cff8ee13e0355efe
Pedro Sa Fontes <pedrosafontes.psf@gmail.com>
2025-02-04 14:43:44 +0000

    feat: add UnitRepository and integrate it into UnitOfWork

--- app/crud/unit_of_work.py
+++ app/crud/unit_of_work.py
@@ -7,6 +7,7 @@ from sqlmodel import Session
 from app.crud.leaderboard_user_repository import LeaderboardUserRepository
 from app.crud.phoneme_repository import PhonemeRepository
 from app.crud.recording_repository import RecordingRepository
+from app.crud.unit_repository import UnitRepository
 from app.crud.user_repository import UserRepository
 from app.crud.word_repository import WordRepository
 from app.database import get_session
@@ -20,6 +21,7 @@ class UnitOfWork:
         self.words = WordRepository(self._session)
         self.phonemes = PhonemeRepository(self._session)
         self.users = UserRepository(self._session)
+        self.units = UnitRepository(self._session)
     
     def __enter__(self) -> Self:
         return self
