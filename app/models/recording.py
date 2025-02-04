8988d08b8c41328177f1cf0fa78305c90fbb1ed9
Pedro Sa Fontes <pedrosafontes.psf@gmail.com>
2025-02-04 21:12:30 +0000

    refactor: rename recording_s3_key to s3_key in Recording model

--- app/models/recording.py
+++ app/models/recording.py
@@ -6,6 +6,6 @@ from app.models.id_model import IdModel
 
 
 class Recording(IdModel, table=True):
-    recording_s3_key: str
+    s3_key: str
     attempt_id: int = Field(foreign_key="attempt.id")
     created_at: datetime = Field(default_factory=datetime.now)
