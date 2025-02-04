efe7121a816ea392b9380b972424c484233d88ba
Pedro Sa Fontes <pedrosafontes.psf@gmail.com>
2025-02-04 11:56:27 +0000

    fix: update Relationship definitions in Phoneme and Word models for bidirectional linking

--- app/models/phoneme.py
+++ app/models/phoneme.py
@@ -10,11 +10,10 @@ from .word_phoneme_link import WordPhonemeLink
 if TYPE_CHECKING:
     from .word import Word
 
-
 # Possible phoneme in each words
 # ipa is the International Phonetic Alphabet representation of the phoneme
 # respelling is a common respelling of the phoneme (wiki respelling)
 class Phoneme(IdModel, table=True):
     ipa: str
     respelling: str
-    words: List["Word"] = Relationship(link_model=WordPhonemeLink)
+    words: List["Word"] = Relationship(back_populates="phonemes", link_model=WordPhonemeLink)
--- app/models/word.py
+++ app/models/word.py
@@ -1,7 +1,17 @@
+from typing import TYPE_CHECKING, List
+
+from sqlmodel import Relationship
 
 from app.models.id_model import IdModel
+from app.models.word_phoneme_link import WordPhonemeLink
+
+
+if TYPE_CHECKING:
+    from app.models.phoneme import Phoneme
 
 
 # Possible words the user can pronounce
 class Word(IdModel, table=True):
     word: str
+
+    phonemes: List["Phoneme"] = Relationship(back_populates="words", link_model=WordPhonemeLink)
