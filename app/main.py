f2fe1953dc314a48ac1f7bb06f5b51a1d77bb5c9
Henry Yu <henrytsyu@gmail.com>
2025-02-04 11:38:54 +0000

    fix: pass lifespan into fastapi app

--- app/main.py
+++ app/main.py
@@ -1,23 +1,12 @@
-from contextlib import asynccontextmanager
-from typing import AsyncGenerator, Dict
+from typing import Dict
 
 from fastapi import FastAPI
 
-from app.cron import scheduler
+from app.cron import lifespan
 from app.routers import routers
 
 
-@asynccontextmanager
-async def lifespan(app: FastAPI) -> AsyncGenerator[None, None]:
-    # Before app startup
-    scheduler.start()
-    yield
-
-    # After app shutdown
-    scheduler.shutdown()
-
-
-app = FastAPI()
+app = FastAPI(lifespan=lifespan)
 
 
 @app.get("/")
