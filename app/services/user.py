dc73b9e5ec13078bd4a1fe5165536a5fda2b59e9
Pedro Sa Fontes <pedrosafontes.psf@gmail.com>
2025-02-05 16:04:18 +0000

    refactor: update update_xp method to accept User object instead of user_id

--- app/services/user.py
+++ app/services/user.py
@@ -1,5 +1,6 @@
 from app.crud.unit_of_work import UnitOfWork
 from app.models.leaderboard_user import LeaderboardUser
+from app.models.user import User
 
 
 class UserService:
@@ -7,9 +8,9 @@ class UserService:
     def __init__(self, uow: UnitOfWork) -> None:
         self._uow = uow
     
-    def update_xp(self, user_id: int, xp_gain: int) -> LeaderboardUser:
+    def update_xp(self, user: User, xp_gain: int) -> LeaderboardUser:
         assert xp_gain >= 0
-        entry = self._uow.leaderboard_users.get_by_user(user_id)
+        entry = user.leaderboard_user
         entry.xp += xp_gain
         entry = self._uow.leaderboard_users.upsert(entry)
         self._uow.commit()
