88d4102cd782bfb5d7944af121c31679af221feb
Henry Yu <henrytsyu@gmail.com>
2025-02-06 18:28:36 +0000

    fix: remove import

--- app/seed.py
+++ app/seed.py
@@ -7,7 +7,7 @@ from sqlmodel import Session, SQLModel, text
 from app.database import engine
 from app.models.attempt import Attempt  # noqa: F401
 from app.models.exercise import Exercise
-from app.models.leaderboard_user_link import LeaderboardUserLink, League  # noqa: F401
+from app.models.leaderboard_user_link import LeaderboardUserLink  # noqa: F401
 from app.models.lesson import Lesson
 from app.models.phoneme import Phoneme
 from app.models.recording import Recording  # noqa: F401
