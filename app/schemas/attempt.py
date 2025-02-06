30e4696f15a55799360c1d973e2c19e2ffe5567e
Pedro Sa Fontes <pedrosafontes.psf@gmail.com>
2025-02-06 19:17:56 +0000

    feat: implement PhonemeService for public phoneme retrieval and update attempt response schema

--- app/schemas/attempt.py
+++ app/schemas/attempt.py
@@ -2,11 +2,11 @@ from typing import List
 
 from pydantic import BaseModel
 
-from app.models.phoneme import Phoneme
+from app.schemas.phoneme import PhonemePublic
 
 
 class AttemptResponse(BaseModel):
     recording_id: int
     score: int
-    recording_phonemes: List[Phoneme]
+    recording_phonemes: List[PhonemePublic]
     xp_gain: int
