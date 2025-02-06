7e10e903ea92b1b7001cc4be1d7bb09a8643017d
Pedro Sa Fontes <pedrosafontes.psf@gmail.com>
2025-02-06 12:15:33 +0000

    refactor: update Recording and Word admin views to use new field names

--- app/admin/recording_admin.py
+++ app/admin/recording_admin.py
@@ -4,5 +4,5 @@ from app.models import Recording
 
 
 class RecordingAdmin(ModelView, model=Recording): # type: ignore[call-arg]
-    column_list = [Recording.id, Recording.recording_s3_key, Recording.user_id, Recording.word_id, Recording.created_at]
-    column_sortable_list = [Recording.id, Recording.recording_s3_key, Recording.user_id, Recording.word_id, Recording.created_at]
+    column_list = [Recording.id, Recording.s3_key, Recording.attempt_id, Recording.created_at]
+    column_sortable_list = [Recording.id, Recording.s3_key, Recording.attempt_id, Recording.created_at]
