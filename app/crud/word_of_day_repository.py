95932ab215c22d1c06954e3ceac1596a0b61ed42
TomSmail <thomas.c.smail@gmail.com>
2025-02-11 22:00:34 +0000

    feat: add cron job to refresh word of day every day

--- app/crud/word_of_day_repository.py
+++ app/crud/word_of_day_repository.py
@@ -10,7 +10,10 @@ class WordOfDayRepository(GenericRepository[WordOfDay]):
     def __init__(self, session: Session):
         super().__init__(session, WordOfDay)
 
-    # Get the latest word of the day
     def get_word_of_day(self) -> WordOfDay:
         stmt = select(WordOfDay).where(WordOfDay.date == date.today())
         return self._session.exec(stmt).one()
+
+    def add_word_of_day(self, word_id: int) -> None:
+        word_of_day = WordOfDay(word_id=word_id)
+        self.upsert(word_of_day)
--- app/crud/word_repository.py
+++ app/crud/word_repository.py
@@ -1,10 +1,19 @@
-from sqlmodel import Session
+from datetime import date, timedelta
+
+from sqlmodel import Session, select
 
 from app.crud.generic_repository import GenericRepository
 from app.models.word import Word
 
 
 class WordRepository(GenericRepository[Word]):
-
     def __init__(self, session: Session):
         super().__init__(session, Word)
+
+    def get_word_not_used_for(self, days: int) -> Word:
+        # TODO: Should this be done atomically?
+        stmt = select(Word).where(Word.word_of_day_last_used < date.today() - timedelta(days=days))
+        res = self._session.exec(stmt).one()
+        res.word_of_day_last_used = date.today()
+        self.upsert(res)
+        return res
