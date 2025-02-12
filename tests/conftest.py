02a5f222fe686600d5812dee1a9bab750bb89ad0
TomSmail <thomas.c.smail@gmail.com>
2025-02-12 22:00:56 +0000

    fix: resolve issues with pytest fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -15,14 +15,15 @@ from .utils import login_user, register_user
 
 
 pytest_plugins = [
-   "tests.fixtures.curriculum_data",
-   "tests.fixtures.leaderboard_data",
+    "tests.fixtures.curriculum_data",
+    "tests.fixtures.leaderboard_data",
+    "tests.fixtures.word_of_day_data",
 ]
+
 
 @pytest.fixture
 def session() -> Iterator[Session]:
-    """Yields an in-memory database session
-    """
+    """Yields an in-memory database session"""
     engine = create_engine("sqlite://", connect_args={"check_same_thread": False}, poolclass=StaticPool)
     SQLModel.metadata.create_all(engine)
 
@@ -32,8 +33,8 @@ def session() -> Iterator[Session]:
 
 @pytest.fixture
 def client(session: Session) -> Iterator[TestClient]:
-    """Yields a TestClient with an in-memory database session override
-    """
+    """Yields a TestClient with an in-memory database session override"""
+
     def get_session_override() -> Session:
         return session
 
@@ -53,8 +54,7 @@ def test_user(session: Session, client: TestClient) -> User:
 
 @pytest.fixture
 def auth_client(client: TestClient, test_user: User) -> TestClient:
-    """Returns a TestClient with a seeded user token
-    """
+    """Returns a TestClient with a seeded user token"""
     user_token = login_user(client, test_user.email, "password").json()["access_token"]
     client.headers = {"Authorization": f"Bearer {user_token}"}
     return client
@@ -62,15 +62,13 @@ def auth_client(client: TestClient, test_user: User) -> TestClient:
 
 @pytest.fixture
 def uow(session: Session) -> Iterator[UnitOfWork]:
-    """Returns a UnitOfWork instance
-    """
+    """Returns a UnitOfWork instance"""
     with UnitOfWork(session) as uow:
         yield uow
 
 
 @pytest.fixture(autouse=True)
 def reset_redis() -> Iterator[None]:
-    """Reset redis after each test
-    """
+    """Reset redis after each test"""
     yield
     LRedis.clear()
