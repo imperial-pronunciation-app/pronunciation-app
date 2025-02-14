bdc1ebf898e64e67ed59f27ef098ff854917fcc5
James Watling <james.watling@gmail.com>
2025-02-14 12:35:20 +0000

    feat: add user dependency and error handling for empty word list in continuous practice endpoint

--- app/routers/continuous_practice.py
+++ app/routers/continuous_practice.py
@@ -1,14 +1,19 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 
 from app.crud.unit_of_work import UnitOfWork, get_unit_of_work
+from app.models.user import User
 from app.schemas.word import WordPublicWithPhonemes
 from app.services.word import WordService
+from app.users import current_active_user
 
 
 router = APIRouter()
 
 @router.get("/api/v1/continuous_practice", response_model=WordPublicWithPhonemes)
-async def get_continuous_practice_word(uow: UnitOfWork = Depends(get_unit_of_work)) -> WordPublicWithPhonemes:
+async def get_continuous_practice_word(uow: UnitOfWork = Depends(get_unit_of_work), user: User = Depends(current_active_user)) -> WordPublicWithPhonemes:
   word_service = WordService(uow)
 
-  return word_service.to_public_with_phonemes(word_service.get_random())
+  try:
+    return word_service.to_public_with_phonemes(word_service.get_random())
+  except IndexError:
+    raise HTTPException(503, "No words are available for practice.")
