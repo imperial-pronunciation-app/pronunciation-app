c6340bfced4d89836e1bafe738da4dfbcf105709
TomSmail <thomas.c.smail@gmail.com>
2025-02-18 12:32:50 +0000

    temp: add temporary work for models attempt

--- app/models/word_of_day.py
+++ app/models/word_of_day.py
@@ -1,6 +1,6 @@
 from datetime import date as pydate
 from datetime import datetime
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, List
 
 from sqlmodel import Field, Relationship
 
@@ -9,10 +9,15 @@ from app.models.id_model import IdModel
 
 if TYPE_CHECKING:
     from app.models.word import Word
+    from app.models.word_of_day_attempt import WordOfDayAttempt
 
 
 class WordOfDay(IdModel, table=True):
+    id: int = Field(default=None, primary_key=True)
     word_id: int = Field(foreign_key="word.id")
     created_at: datetime = Field(default_factory=datetime.now)
     date: pydate = Field(index=True, default_factory=pydate.today)
     word: "Word" = Relationship(back_populates="word_of_day")
+    word_of_day_attempts: List["WordOfDayAttempt"] = Relationship(
+        back_populates="word_of_day", sa_relationship_kwargs={"cascade": "all, delete-orphan"}
+    )
