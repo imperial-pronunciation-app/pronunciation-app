72726d41363440bd0187fb43528d2db02596a4ac
TomSmail <thomas.c.smail@gmail.com>
2025-02-18 13:25:52 +0000

    fix: begin to change mocking to use service

--- tests/test_attempts.py
+++ tests/test_attempts.py
@@ -11,17 +11,17 @@ from app.models.recording import Recording
 from app.models.unit import Unit
 from app.models.word import Word
 from app.models.word_phoneme_link import WordPhonemeLink
-from app.routers.attempts import create_wav_file
+from app.services.attempts import AttemptService
 from app.utils.similarity import similarity
 
 
-def test_create_wav_file(mocker: MockerFixture) -> None:
+def test_create_wav_file(mocker: MockerFixture, uow: UnitOfWork) -> None:
     audio_bytes = b"test"
 
     mock_file = mocker.mock_open()
     mocker.patch("builtins.open", mock_file)
 
-    filename = create_wav_file(audio_bytes)
+    filename = AttemptService(uow).create_wav_file(audio_bytes)
 
     mock_file.assert_called_once_with(filename, "bx")
     mock_file().write.assert_called_once_with(audio_bytes)
@@ -42,28 +42,26 @@ def test_similarity(s1: list[str], s2: list[str], expected: int) -> None:
     assert similarity(s1, s2) == expected
 
 
-def test_post_attempt(
-        session: Session,
-        uow: UnitOfWork,
-        mocker: MockerFixture,
-        auth_client: TestClient
-    ) -> None:
+def test_post_attempt(session: Session, uow: UnitOfWork, mocker: MockerFixture, auth_client: TestClient) -> None:
     # Check that calls to:
     # create_wav_file, upload_wav_to_s3, dispatch_to_model, similarity are made correctly
     # Recording entry is added to the table
     # File is deleted correctly
     # Correct response is returned
     test_word = "software"
-    test_word_phonemes = ['s', 'oʊ', 'f', 't', 'w', 'ɛ', 'r']
+    test_word_phonemes = ["s", "oʊ", "f", "t", "w", "ɛ", "r"]
     blob_id = "blob_id"
     test_wav_filename = "test.wav"
     similarity = 100
 
-    mock_create_wav_file = mocker.patch("app.routers.attempts.create_wav_file", return_value=test_wav_filename)
-    mock_upload_wav_to_s3 = mocker.patch("app.routers.attempts.upload_wav_to_s3", return_value=blob_id)
-    mock_dispatch_to_model = mocker.patch("app.routers.attempts.dispatch_to_model", return_value=test_word_phonemes)
-    mock_os_remove = mocker.patch("os.remove")
-    mock_similarity = mocker.patch("app.routers.attempts.similarity", return_value=similarity)
+    # mock_os_remove = mocker.patch("os.remove")
+    mock_service = mocker.Mock(spec=AttemptService)
+    mocker.patch("app.routers.attempts.AttemptService", return_value=mock_service)
+    mock_upload_wav_to_s3 = mocker.patch("app.utils.s3.upload_wav_to_s3", return_value=blob_id)
+    mock_calculate_similarity = mocker.patch("app.utils.similarity.similarity", return_value=similarity)
+
+    mock_service.create_wav_file.return_value = test_wav_filename
+    mock_service.dispatch_to_model.return_value = test_word_phonemes
 
     word = uow.words.upsert(Word(text=test_word))
     phonemes = uow.phonemes.upsert_all([Phoneme(ipa=p, respelling=p) for p in test_word_phonemes])
@@ -79,23 +77,19 @@ def test_post_attempt(
     with open(wav_file_path, "rb") as f:
         files = {"audio_file": f}
 
-        recording_response = auth_client.post(
-            f"/api/v1/exercises/{exercise.id}/attempts",
-            files=files
-        )
+        recording_response = auth_client.post(f"/api/v1/exercises/{exercise.id}/attempts", files=files)
     assert recording_response.status_code == 200
     data = recording_response.json()
+    print(f"\nThe data is: {data}")
     assert data["score"] == 100
     assert data["xp_gain"] == 1.5 * 100
     recording_id = data["recording_id"]
-    
+
     recording = session.get(Recording, recording_id)
     # TODO: Test the phonemes
     assert recording is not None
-    
-    mock_create_wav_file.assert_called_once()
+
+    mock_service.create_wav_file.assert_called_once()
     mock_upload_wav_to_s3.assert_called_once_with(test_wav_filename)
-    
-    mock_dispatch_to_model.assert_called_once_with(test_wav_filename)
-    mock_os_remove.assert_called_once_with(test_wav_filename)
-    mock_similarity.assert_called_once_with(test_word_phonemes, test_word_phonemes)
+    mock_service.dispatch_to_model.assert_called_once_with(test_wav_filename)
+    mock_calculate_similarity.assert_called_once_with(test_word_phonemes, test_word_phonemes)
