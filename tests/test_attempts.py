c35e120bcf1c865aec8251d7f080171e70907fa6
Pedro Sá Fontes <pedrosafontes.psf@gmail.com>
2025-02-19 12:02:08 +0000

    Feedback (#69)
    * chore: remove unused network configuration from docker-compose.override.yml
    
    * fix: update API endpoint for phoneme inference to include language prefix
    
    * fix: correct phoneme representation for 'hardware'
    
    * feat: replace similarity function with sequence alignment function
    
    * feat: add phoneme similarity resource and utility function
    
    * feat: implement pronunciation evaluation service, update post attempt router to call service and return aligned phonemes
    
    * feat: add pronunciation data fixtures and tests for pronunciation evaluation
    
    * refactor: update tests to use PronunciationService and remove similarity function
    
    * refactor: rename parameters in evaluate_pronunciation for clarity

--- tests/test_attempts.py
+++ tests/test_attempts.py
@@ -1,9 +1,18 @@
 import pytest
+from fastapi.testclient import TestClient
 from pytest_mock import MockerFixture
 
 from app.crud.unit_of_work import UnitOfWork
 from app.services.attempts import AttemptService
-from app.utils.similarity import similarity
+from app.models.exercise import Exercise
+from app.models.lesson import Lesson
+from app.models.phoneme import Phoneme
+from app.models.recording import Recording
+from app.models.unit import Unit
+from app.models.word import Word
+from app.models.word_phoneme_link import WordPhonemeLink
+from app.routers.attempts import create_wav_file
+from app.services.pronunciation import PronunciationService
 
 
 def test_create_wav_file(mocker: MockerFixture, uow: UnitOfWork) -> None:
@@ -16,19 +25,4 @@ def test_create_wav_file(mocker: MockerFixture, uow: UnitOfWork) -> None:
 
     mock_file.assert_called_once_with(filename, "bx")
     mock_file().write.assert_called_once_with(audio_bytes)
-
-
-@pytest.mark.parametrize(
-    "s1, s2, expected",
-    [
-        (["a", "b", "c"], ["a", "b", "c"], 100),
-        (["a", "b", "c"], ["d", "e", "f"], 0),
-        (["a", "b", "c"], ["a", "b", "d"], 67),
-        (["a", "b", "c"], ["a", "b", "c", "d"], 75),
-        (["a", "b", "c"], ["a", "c", "b", "d"], 50),
-        (["a", "b", "c"], ["c", "a", "b"], 33),
-    ],
-)
-def test_similarity(s1: list[str], s2: list[str], expected: int) -> None:
-    assert similarity(s1, s2) == expected
 
