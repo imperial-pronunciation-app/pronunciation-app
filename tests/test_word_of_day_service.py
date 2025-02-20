7415e4f35b57673bb80fabd3ddf03d60d1722f2e
TomSmail <thomas.c.smail@gmail.com>
2025-02-20 12:42:54 +0000

    feat: add test for correct date

--- tests/test_word_of_day_service.py
+++ tests/test_word_of_day_service.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 from app.crud.unit_of_work import UnitOfWork
 from app.models.word import Word
 from app.services.word_of_day import WordOfDayService
@@ -13,3 +15,15 @@ def test_assign_new_word_of_day(uow: UnitOfWork, sample_word: Word) -> None:
     word_of_day = uow.word_of_day.get_word_of_day()
     assert word_of_day.id is sample_word.id
     assert word_of_day.word_id is sample_word.id
+
+
+def test_word_of_day_assigned_as_such(uow: UnitOfWork, sample_word: Word) -> None:
+    """Tests that the word of the day is assigned the correct date"""
+
+    # When
+    WordOfDayService(uow).change_word_of_day()
+
+    # Then
+    word_of_day = uow.word_of_day.get_word_of_day()
+    assert word_of_day.date == date.today()
+    assert word_of_day.word_id == sample_word.id
