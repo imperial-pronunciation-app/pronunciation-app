62742e563bab8d10ae6ea087fcbcea4c85b3ea91
James Watling <james.watling@gmail.com>
2025-02-20 22:41:37 +0000

    feat: Include recap lesson in unit response if it exists.

--- app/crud/lesson_repository.py
+++ app/crud/lesson_repository.py
@@ -1,5 +1,7 @@
 
-from sqlmodel import Session
+from typing import Optional
+
+from sqlmodel import Session, select
 
 from app.crud.generic_repository import GenericRepository
 from app.models.lesson import Lesson
@@ -9,5 +11,12 @@ class LessonRepository(GenericRepository[Lesson]):
 
     def __init__(self, session: Session):
         super().__init__(session, Lesson)
+
+    def find_recap_by_user_id_and_unit_id(self, unit_id: int, user_id: int) -> Optional[Lesson]:
+        stmt = (
+            select(Lesson)
+            .where(Lesson.unit_id == unit_id, Lesson.user_id == user_id)
+        )
+        return self._session.exec(stmt).first()
 
     
--- app/services/unit.py
+++ app/services/unit.py
@@ -17,8 +17,9 @@ class UnitService:
             name=unit.name,
             description=unit.description,
             lessons=[lesson_service.to_response(lesson, user) for lesson in unit.lessons],
-            recap_lesson=None
+            recap_lesson=self._uow.lessons.find_recap_by_user_id_and_unit_id(user.id, unit.id)
         )
+        
 
     def generate_recap_lesson(self, unit: Unit, user: User) -> Lesson:
         exercise_scores = [(exercise, self._uow.attempts.get_max_score_by_user_id_and_exercise_id(user.id, exercise.id)) for lesson in unit.lessons for exercise in lesson.exercises]
