8b36526a994c53eb38c9b8a6991e99649c5e6b75
TomSmail <thomas.c.smail@gmail.com>
2025-02-24 19:02:18 +0000

    fix: remove error endpoint

--- app/main.py
+++ app/main.py
@@ -33,12 +33,6 @@ def read_home() -> Dict[str, str]:  # would be a Pydantic return type normally
     return {"Hello": "James"}
 
 
-# TODO: REMOVE THIS!!!!
-@app.get("/error")
-def divide_by_zero() -> float:
-    return 1 / 0
-
-
 for router in routers:
     app.include_router(router)
 
