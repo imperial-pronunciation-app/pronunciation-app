783f3695a093474619c2d87ecd6755236627fb2b
Pedro Sá Fontes <pedrosafontes.psf@gmail.com>
2025-02-26 18:10:09 +0000

    Locked units and lessons (#87)
    * refactor: rename 'order' to 'index' in Unit model and make it 0 indexed
    
    * feat: add method to calculate average max score for a lesson by user
    
    * feat: lock lessons and units, include unit completion in response
    
    * feat: add stars attribute to LessonResponse and implement star calculation based on average score
    
    * feat: update endpoint for word of day attempts to include 'attempts' in the URL
    
    * refactor: improve recap lesson response handling in UnitService

--- app/services/lesson.py
+++ app/services/lesson.py
@@ -1,7 +1,7 @@
+from typing import Optional
+
 from app.crud.unit_of_work import UnitOfWork
-from app.models.basic_lesson import BasicLesson
 from app.models.lesson import Lesson
-from app.models.recap_lesson import RecapLesson
 from app.models.user import User
 from app.schemas.lesson import LessonResponse
 from app.services.exercise import ExerciseService
@@ -10,28 +10,31 @@ from app.services.exercise import ExerciseService
 class LessonService:
     def __init__(self, uow: UnitOfWork):
         self._uow = uow
-
-    def basic_to_response(self, basic: BasicLesson, user: User) -> LessonResponse:
-        lesson = self._uow.lessons.get_by_id(basic.id)
-        return self.to_response(lesson, user)
     
-    def recap_to_response(self, recap: RecapLesson, user: User) -> LessonResponse:
-        lesson = self._uow.lessons.get_by_id(recap.id)
-        return self.to_response(lesson, user)
-    
-    def to_response(self, lesson: Lesson, user: User) -> LessonResponse:
+    def to_response(self, lesson: Lesson, user: User, prev_lesson: Optional[Lesson] = None) -> LessonResponse:
+        is_completed = self._is_completed_by(lesson, user)
         return LessonResponse(
             id=lesson.id,
             title=lesson.title,
-            is_completed=self._is_completed_by(lesson, user),
-            first_exercise_id=lesson.exercises[0].id
+            is_completed=is_completed,
+            first_exercise_id=lesson.exercises[0].id,
+            is_locked=prev_lesson is not None and not self._is_completed_by(prev_lesson, user),
+            stars=self._stars_for(lesson, user) if is_completed else None
         )
+    
+    def _stars_for(self, lesson: Lesson, user: User) -> int:
+        """Returns the number of stars (0 to 3) earned by the user for this lesson."""
+        average_score = self._uow.exercise_attempts.average_max_score_for_lesson(user.id, lesson.id)
+        if average_score >= 0.9:
+            return 3
+        elif average_score >= 0.7:
+            return 2
+        elif average_score >= 0.5:
+            return 1
+        else:
+            return 0
     
     def _is_completed_by(self, lesson: Lesson, user: User) -> bool:
         """Returns True if the user has completed this lesson. i.e. if all exercises were attempted"""
         exercise_service = ExerciseService(self._uow)
         return all(exercise_service.is_completed_by(exercise, user) for exercise in lesson.exercises)
-
-    def _is_last_lesson(self, lesson: BasicLesson) -> bool:
-        """Returns True if this is the last lesson within the unit."""
-        return lesson.index == len(lesson.unit.lessons) - 1
