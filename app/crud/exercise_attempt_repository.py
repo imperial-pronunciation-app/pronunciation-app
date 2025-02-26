8a5c0658870c82fce6a75518754a5458343ac864
Pedro Sa Fontes <pedrosafontes.psf@gmail.com>
2025-02-26 17:45:30 +0000

    feat: add method to calculate average max score for a lesson by user

--- app/crud/exercise_attempt_repository.py
+++ app/crud/exercise_attempt_repository.py
@@ -1,9 +1,10 @@
 from typing import Sequence
 
-from sqlmodel import Session, select
+from sqlmodel import Session, col, func, select
 
 from app.crud.generic_repository import GenericRepository
 from app.models.attempt import Attempt
+from app.models.exercise import Exercise
 from app.models.exercise_attempt import ExerciseAttempt
 
 
@@ -21,3 +22,23 @@ class ExerciseAttemptRepository(GenericRepository[ExerciseAttempt]):
 
     def get_max_score_by_user_id_and_exercise_id(self, user_id: int, exercise_id: int) -> int:
         return max([attempt.attempt.score for attempt in self.find_by_user_id_and_exercise_id(user_id, exercise_id)])
+    
+    def average_max_score_for_lesson(self, user_id: int, lesson_id: int) -> float:
+        subquery = (
+            select(
+                ExerciseAttempt.exercise_id,
+                func.max(Attempt.score).label("max_score")
+            )
+            .join(Exercise)
+            .join(Attempt)
+            .where(
+                Attempt.user_id == user_id,
+                Exercise.lesson_id == lesson_id
+            )
+            .group_by(col(ExerciseAttempt.exercise_id))
+            .subquery()
+        )
+
+        return self._session.exec(
+            select(func.avg(subquery.c.max_score))
+        ).first() or 0.0
