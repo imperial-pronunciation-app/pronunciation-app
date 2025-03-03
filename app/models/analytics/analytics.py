07268f13afcadce38b3a16da862f543948a18cdf
TomSmail <thomas.c.smail@gmail.com>
2025-03-03 17:58:11 +0000

    feat: add defaults

--- app/models/analytics/analytics.py
+++ app/models/analytics/analytics.py
@@ -1,5 +1,7 @@
 from datetime import datetime
 
+from sqlmodel import Field
+
 from app.models.analytics.http_method import HTTPMethod
 from app.models.id_model import IdModel
 
@@ -8,5 +10,5 @@ class EndpointAnalytics(IdModel, table=True):
     endpoint: str
     method: HTTPMethod
     status_code: int
-    duration: float
-    timestamp: datetime
+    duration: float = Field(nullable=False, default=0)
+    timestamp: datetime = Field(default_factory=datetime.now, nullable=False)
