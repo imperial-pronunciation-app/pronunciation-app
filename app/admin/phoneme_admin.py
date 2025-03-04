a5be9cd6bb4eefe72558623dbe38eaaea030c9d2
Euan Turner <euanoturner@gmail.com>
2025-03-04 18:52:41 +0000

    feat(Phonemes): :sparkles: Add cdn paths for phonemes

--- app/admin/phoneme_admin.py
+++ app/admin/phoneme_admin.py
@@ -4,6 +4,6 @@ from app.models import Phoneme
 
 
 class PhonemeAdmin(ModelView, model=Phoneme): # type: ignore[call-arg]
-    column_list = [Phoneme.id, Phoneme.ipa, Phoneme.respellings, Phoneme.words]
-    column_searchable_list = [Phoneme.ipa]
+    column_list = [Phoneme.id, Phoneme.ipa, Phoneme.cdn_path, Phoneme.respellings, Phoneme.words]
+    column_searchable_list = [Phoneme.ipa, Phoneme.cdn_path]
     column_sortable_list = [Phoneme.id, Phoneme.ipa]
--- app/models/base/phoneme_base.py
+++ app/models/base/phoneme_base.py
@@ -3,3 +3,4 @@ from app.models.id_model import IdModel
 
 class PhonemeBase(IdModel):
     ipa: str
+    cdn_path: str
